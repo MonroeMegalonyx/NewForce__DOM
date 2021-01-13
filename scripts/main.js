4ee017910889bdfd3cdcb4af8b0c7bc1dc3d2255
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-13 15:11:19 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -77,38 +77,38 @@
 //      genre: "Science Fiction",
 //      releaseDate: "May 25, 1977",
 //  };
-//  
+//
 //  // Use document.querySelector() to grab a reference to each element in your HTML.
 //  // Use the .innerHTML property to set the text of each element to the matching value in your object. (For example, the h1 with the id of "title" should contain the title property of your movieObject, etc).
-//  
+//
 //  document.querySelector("#title").innerHTML = `${movieObject.title}`;
 //  document.querySelector("#genre").innerHTML = `${movieObject.genre}`;
 //  document.querySelector("#release-date").innerHTML = `${movieObject.releaseDate}`;
-//  
+//
 //  // 2. Downtown Restuarants
-//  
+//
 //  let downtownRestaurants = ["Backyard Pizza and Raw Bar", "The Bodega", "Surin of Thailand", "Bahnhof", "Jim's Steak and Spaghetti House", "The Peddler", "Jewel City Seafood Market", "Black Sheep Burritos & Brews", "La Famiglia", "Le Bistro", "Charlie Graingers", "The Cellar Door"];
-//  
+//
 //  // Loop over the downtownRestaurants array.
 //  // Use the loop to create an <li> element for each item in the array.
 //  // Use document.querySelector and .innerHTML to add each new <li> element to the #restaurants-list container.
 //  // You should see a bulleted list of restaurants in the DOM.
-//  
+//
 //  for (let i = 0; i < downtownRestaurants.length; i++) {
 //      document.querySelector(
 //          "#restaurants-list"
 //      ).innerHTML += `<li>${downtownRestaurants[i]}</li>`;
 //  }
-//  
+//
 //  // 3. Sandwich Toppings
-//  
+//
 //  let sandwichToppings = ["pickles", "extra cheese", "lettuce", "tomatoes", "avocado", "bacon", "mayo", "mustard"]
-//  
+//
 //  // Write a function that does the following things:
 //  // Loop through sandwichToppings
 //  // Build an <li> element for each item in the array.
 //  // Use document.querySelector and .innerHTML to print your HTML string to the "#sandwich-container".
-//  
+//
 //  function sandwichTopper(){
 //      for (let i = 0; i < sandwichToppings.length; i++) {
 //          document.querySelector(
@@ -116,9 +116,9 @@
 //          ).innerHTML += `<li>${sandwichToppings[i]}</li>`;
 //      }
 //  }
-//  
+//
 //  // Call the function. You should see a bulleted list of all the sandwich toppings in the DOM.
-//  
+//
 //  sandwichTopper()
 
 // 4. Movie Schedule
@@ -126,46 +126,51 @@
 
 let movieSchedule = [
     {
-      title: "Ralph Breaks the Internet",
-      rating: "PG",
-      currentlyPlaying: true,
-      poster: "https://lumiere-a.akamaihd.net/v1/images/r_ralphbreakstheinternet_header_ddt-17403_08ef6d92.jpeg?region=0,0,1024,1274&optimize=true"
+        title: "Ralph Breaks the Internet",
+        rating: "PG",
+        currentlyPlaying: true,
+        poster:
+            "https://lumiere-a.akamaihd.net/v1/images/r_ralphbreakstheinternet_header_ddt-17403_08ef6d92.jpeg?region=0,0,1024,1274&optimize=true",
     },
     {
-      title: "The Grinch",
-      rating: "G",
-      currentlyPlaying: true,
-      poster: "https://images-na.ssl-images-amazon.com/images/I/71vgX4VFdiL._SY679_.jpg"
+        title: "The Grinch",
+        rating: "G",
+        currentlyPlaying: true,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/I/71vgX4VFdiL._SY679_.jpg",
     },
     {
-      title: "A Star is Born",
-      rating: "R",
-      currentlyPlaying: false,
-      poster: "https://images-na.ssl-images-amazon.com/images/I/718zWbDKmvL._SY606_.jpg"
-    },
-     {
-      title: "Bohemian Rhapsody",
-      rating: "PG-13",
-      currentlyPlaying: true
+        title: "A Star is Born",
+        rating: "R",
+        currentlyPlaying: false,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/I/718zWbDKmvL._SY606_.jpg",
     },
     {
-      title: "Fantastic Beasts: The Crimes of Grindlewald",
-      rating: "PG-13",
-      currentlyPlaying: true,
-      poster: "https://images-na.ssl-images-amazon.com/images/I/716fk%2BBZ-NL._SY606_.jpg"
+        title: "Bohemian Rhapsody",
+        rating: "PG-13",
+        currentlyPlaying: true,
     },
     {
-      title: "Robin Hood",
-      rating: "PG-13",
-      currentlyPlaying: false,
-      poster: "https://images-na.ssl-images-amazon.com/images/I/61K%2BTyGjsCL.jpg"
+        title: "Fantastic Beasts: The Crimes of Grindlewald",
+        rating: "PG-13",
+        currentlyPlaying: true,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/I/716fk%2BBZ-NL._SY606_.jpg",
     },
     {
-      title: "Spider-Man: Into the Spider-Verse",
-      rating: "PG-13",
-      currentlyPlaying: true,
-    }
-  ]
+        title: "Robin Hood",
+        rating: "PG-13",
+        currentlyPlaying: false,
+        poster:
+            "https://images-na.ssl-images-amazon.com/images/I/61K%2BTyGjsCL.jpg",
+    },
+    {
+        title: "Spider-Man: Into the Spider-Verse",
+        rating: "PG-13",
+        currentlyPlaying: true,
+    },
+];
 
 // Use a for loop to loop through the movies, build an HTML representation of each movie, and insert it into the #movie-schedule container in the DOM.
 // You can use any elements you like in your HTML string as long as all the data in the movieSchedule object is included.
@@ -177,16 +182,21 @@ let movieSchedule = [
 
 for (let i = 0; i < movieSchedule.length; i++) {
     // If its currently playing and has a poster, add it to index
-    if (movieSchedule[i].hasOwnProperty("poster") && movieSchedule[i].currentlyPlaying) {
-        document.querySelector("#movie-schedule").innerHTML += 
-        `<section class="moviemovie"><h1>${movieSchedule[i].title}</h1><p>RATING: ${movieSchedule[i].rating}</p><b>GET YOUR TICKETS NOW</b><img src=${movieSchedule[i].poster} class="${movieSchedule[i].rating}">`        
-    } else
-        // If its currently playing but has no poster, add a placeholder and put it on the index
-        if (movieSchedule[i].currentlyPlaying) {
-            document.querySelector("#movie-schedule").innerHTML += 
-            `<section class="moviemovie"><h1>${movieSchedule[i].title}</h1><p>RATING: ${movieSchedule[i].rating}</p><b>GET YOUR TICKETS NOW</b><img src="https://www.snhrc.com/wp-content/uploads/2018/09/Image-Coming-Soon.png">`;
-        }; // If its not currently playing then dont add it to the index
-    };
+    if (
+        movieSchedule[i].hasOwnProperty("poster") &&
+        movieSchedule[i].currentlyPlaying
+    ) {
+        document.querySelector(
+            "#movie-schedule"
+        ).innerHTML += `<section class="moviemovie"><h1>${movieSchedule[i].title}</h1><p>RATING: ${movieSchedule[i].rating}</p><b>GET YOUR TICKETS NOW</b><img src=${movieSchedule[i].poster} class="${movieSchedule[i].rating}">`;
+    }
+    // If its currently playing but has no poster, add a placeholder and put it on the index
+    else if (movieSchedule[i].currentlyPlaying) {
+        document.querySelector(
+            "#movie-schedule"
+        ).innerHTML += `<section class="moviemovie"><h1>${movieSchedule[i].title}</h1><p>RATING: ${movieSchedule[i].rating}</p><b>GET YOUR TICKETS NOW</b><img src="https://www.snhrc.com/wp-content/uploads/2018/09/Image-Coming-Soon.png">`;
+    } // If its not currently playing then dont add it to the index
+}
 
 // Exercise 5. Movie Scheduling cont. I made the edits above for this part.
 
@@ -199,4 +209,4 @@ for (let i = 0; i < movieSchedule.length; i++) {
 // Give all PG-13 movies a dotted orange border that's 2px wide.
 // Give all R-rated movies a dotted red border that's 2px wide.
 
-// Exercise 6. 
+// Exercise 6.
