bf21390d133eb932f906af4ddae4fb6ae575e02a
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-13 13:25:18 -0500

    Finished Exercise 3

--- scripts/main.js
+++ scripts/main.js
@@ -87,3 +87,103 @@ document.querySelector("#release-date").innerHTML = `${movieObject.releaseDate}`
 
 // 2. Downtown Restuarants
 
+let downtownRestaurants = ["Backyard Pizza and Raw Bar", "The Bodega", "Surin of Thailand", "Bahnhof", "Jim's Steak and Spaghetti House", "The Peddler", "Jewel City Seafood Market", "Black Sheep Burritos & Brews", "La Famiglia", "Le Bistro", "Charlie Graingers", "The Cellar Door"];
+
+// Loop over the downtownRestaurants array.
+// Use the loop to create an <li> element for each item in the array.
+// Use document.querySelector and .innerHTML to add each new <li> element to the #restaurants-list container.
+// You should see a bulleted list of restaurants in the DOM.
+
+for (let i = 0; i < downtownRestaurants.length; i++) {
+    document.querySelector(
+        "#restaurants-list"
+    ).innerHTML += `<li>${downtownRestaurants[i]}</li>`;
+}
+
+// 3. Sandwich Toppings
+
+let sandwichToppings = ["pickles", "extra cheese", "lettuce", "tomatoes", "avocado", "bacon", "mayo", "mustard"]
+
+// Write a function that does the following things:
+// Loop through sandwichToppings
+// Build an <li> element for each item in the array.
+// Use document.querySelector and .innerHTML to print your HTML string to the "#sandwich-container".
+
+function sandwichTopper(){
+    for (let i = 0; i < sandwichToppings.length; i++) {
+        document.querySelector(
+            "#sandwich-container"
+        ).innerHTML += `<li>${sandwichToppings[i]}</li>`;
+    }
+}
+
+// Call the function. You should see a bulleted list of all the sandwich toppings in the DOM.
+
+sandwichTopper()
+
+// 4. Movie Schedule
+// You've been hired to build an app for a movie theater. You need to list all of the movies that are currently playing.
+
+let movieSchedule = [
+    {
+      title: "Ralph Breaks the Internet",
+      rating: "PG",
+      currentlyPlaying: true,
+      poster: "https://lumiere-a.akamaihd.net/v1/images/r_ralphbreakstheinternet_header_ddt-17403_08ef6d92.jpeg?region=0,0,1024,1274&optimize=true"
+    },
+    {
+      title: "The Grinch",
+      rating: "G",
+      currentlyPlaying: true,
+      poster: "https://images-na.ssl-images-amazon.com/images/I/71vgX4VFdiL._SY679_.jpg"
+    },
+    {
+      title: "A Star is Born",
+      rating: "R",
+      currentlyPlaying: false,
+      poster: "https://images-na.ssl-images-amazon.com/images/I/718zWbDKmvL._SY606_.jpg"
+    },
+     {
+      title: "Bohemian Rhapsody",
+      rating: "PG-13",
+      currentlyPlaying: true
+    },
+    {
+      title: "Fantastic Beasts: The Crimes of Grindlewald",
+      rating: "PG-13",
+      currentlyPlaying: true,
+      poster: "https://images-na.ssl-images-amazon.com/images/I/716fk%2BBZ-NL._SY606_.jpg"
+    },
+    {
+      title: "Robin Hood",
+      rating: "PG-13",
+      currentlyPlaying: false,
+      poster: "https://images-na.ssl-images-amazon.com/images/I/61K%2BTyGjsCL.jpg"
+    },
+    {
+      title: "Spider-Man: Into the Spider-Verse",
+      rating: "PG-13",
+      currentlyPlaying: true,
+    }
+  ]
+
+// Use a for loop to loop through the movies, build an HTML representation of each movie, and insert it into the #movie-schedule container in the DOM.
+// You can use any elements you like in your HTML string as long as all the data in the movieSchedule object is included.
+
+// I will need to get the output similar to exercise 1, but by using loops:
+// document.querySelector("#title").innerHTML = `${movieObject.title}`;
+// document.querySelector("#genre").innerHTML = `${movieObject.genre}`;
+// document.querySelector("#release-date").innerHTML = `${movieObject.releaseDate}
+
+for (let i = 0; i < movieSchedule.length; i++) {
+    
+    if (movieSchedule[i].hasOwnProperty("poster")) {
+    document.querySelector(
+        "#movie-schedule"
+    ).innerHTML += `<h1>${movieSchedule[i].title}</h1><p>RATING: ${movieSchedule[i].rating}</p><i>Is it true its playing today? That's ${movieSchedule[i].currentlyPlaying}</i> <img src=${movieSchedule[i].poster} width = 100>`;
+    } else document.querySelector(
+        "#movie-schedule"
+    ).innerHTML += `<h1>${movieSchedule[i].title}</h1><p>${movieSchedule[i].rating}</p><i>Is it true its playing today? That's ${movieSchedule[i].currentlyPlaying}`;
+}
+
+// I want to only include posters for movies that have them, but the for loop ends the first time the if statement fails
