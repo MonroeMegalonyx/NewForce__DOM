8de87aeda069318856ba5ea3832800826940ae9b
Vincent OLeary <7621080+MonroeMegalonyx@users.noreply.github.com>
2021-01-13 10:40:07 -0500

    finished practice exercise

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,14 @@
-let headingElement = document.querySelector("#main-heading");
+// LIGHTNING EXERCISE
+// Hello, world
+///In your index.html file, add an h1 element with an id of #hello-container.
+// In your JavaScript file, declare a new variable called greeting and give it a value of "Hello, world"
 
-headingElement.innerHTML = "Hello, world!";
+let greeting = "Hello, world";
+
+// Use document.querySelector to target your #hello-container
+
+let targetHelloContainer = document.querySelector("#hello-container");
+
+// Use the .innerHTML property to insert the value of your greeting variable into your #hello-container element.
+
+targetHelloContainer.innerHTML = greeting;
